f8601ced8aed11642a9ff9da7dfbdb3f79409a58
Shuhei YAMADA <duklyon.com@gmail.com>
2019-01-26 23:46:45 +0900

    feat:タイトル画面とゲームオーバー画面を実装した

--- SomethingForgotten/Assets/Q/Scripts/GameController.cs
+++ SomethingForgotten/Assets/Q/Scripts/GameController.cs
@@ -5,72 +5,89 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    static GameController mInstance;
-    
-    public static GameController Instance
-    {
-        get
-        {
-            return mInstance;
-        }
-    }
+	static GameController mInstance;
+	
+	public static GameController Instance
+	{
+		get
+		{
+			return mInstance;
+		}
+	}
 
-    public GameObject StartPanel;
-    public GameObject GameOverPanel;
-    public Text GameOverMessage;
+	public GameObject StartPanel;
+	public GameObject GameOverPanel;
+	public Text GameOverMessage;
+	public Text GameClearMessage;
 
-    void Awake()
-    {
-        mInstance = this;   
-    }
+	private bool _canStart = true;
+	private bool _canRestart = false;
 
-    void Start()
-    {
-        GameReady();
-    }
+	void Awake()
+	{
+		mInstance = this;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		GameReady();
+	}
 
-    public void GameOver()
-    {
-        Time.timeScale = 0;
-        GameOverPanel.SetActive(true);
-        GameOverMessage.text = "GameOver!";
-    }
+	void Update()
+	{
+		if (_canStart && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Joystick1Button0)))
+		{
+			GameStart();
+			_canStart = false;
+		}
 
-    public void GameClear()
-    {
-        Time.timeScale = 0;
-        StartPanel.SetActive(false);
-        GameOverPanel.SetActive(true);
-        GameOverMessage.text = "Game Clear!";
-    }
+		if (_canRestart && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Joystick1Button0)))
+		{
+			GameRestart();
+			_canRestart = false;
+		}
+	}
 
-    public void GameReady()
-    {
-        Time.timeScale = 0;
-        StartPanel.SetActive(true);
-        GameOverPanel.SetActive(false);
-    }
+	public void GameOver()
+	{
+		Time.timeScale = 0;
+		GameOverPanel.SetActive(true);
+		GameOverMessage.gameObject.SetActive(true);
+		GameClearMessage.gameObject.SetActive(false);
+		_canRestart = true;
+	}
 
-    public void GameStart()
-    {
-        Time.timeScale = 1;
-        StartPanel.SetActive(false);
-        GameOverPanel.SetActive(false);
-    }
+	public void GameClear()
+	{
+		Time.timeScale = 0;
+		StartPanel.SetActive(false);
+		GameOverPanel.SetActive(true);
+		GameOverMessage.gameObject.SetActive(false);
+		GameClearMessage.gameObject.SetActive(true);
+		_canRestart = true;
+	}
 
-    public void GameRestart()
-    {
-        Application.LoadLevel(Application.loadedLevel);
-    }
+	public void GameReady()
+	{
+		Time.timeScale = 0;
+		StartPanel.SetActive(true);
+		GameOverPanel.SetActive(false);
+	}
 
-    public void GameExit()
-    {
-        Application.Quit();
-    }
+	public void GameStart()
+	{
+		Time.timeScale = 1;
+		StartPanel.SetActive(false);
+		GameOverPanel.SetActive(false);
+	}
+
+	public void GameRestart()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void GameExit()
+	{
+		Application.Quit();
+	}
 }
