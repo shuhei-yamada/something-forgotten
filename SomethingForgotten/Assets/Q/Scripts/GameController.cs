ac84aa00c5fc89a4d1644a338ebe8899935247cb
LinsIan <jjes20055114123@gmail.com>
2019-01-26 17:55:00 +0900

    Add GameController
    Signed-off-by: LinsIan <jjes20055114123@gmail.com>

---
+++ SomethingForgotten/Assets/Q/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private static GameController mInstance;
+    
+    public static GameController Instance
+    {
+        get
+        {
+            if(mInstance == null)
+            {
+                mInstance = new GameController();
+            }
+            return mInstance;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
