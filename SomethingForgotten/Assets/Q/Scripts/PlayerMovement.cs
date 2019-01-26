44c89cb5000cb53945e7f16823725ba8ecacc7aa
LinsIan <jjes20055114123@gmail.com>
2019-01-26 14:43:38 +0900

    Add Player & Camera Movement
    Signed-off-by: LinsIan <jjes20055114123@gmail.com>

---
+++ SomethingForgotten/Assets/Q/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    Vector3 Movement;
+    Rigidbody PlayerRigidbody;
+
+    void Start()
+    {
+       PlayerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float HorizontalInput = Input.GetAxisRaw("Horizontal");
+        float VerticalInput = Input.GetAxisRaw("Vertical");
+
+        Move(HorizontalInput,VerticalInput);
+    }
+
+    void Move(float HorizontalInput,float VerticalInput)
+    {
+        Movement.Set(HorizontalInput,0,VerticalInput);
+        Movement = Movement.normalized * MoveSpeed * Time.deltaTime;
+        PlayerRigidbody.MovePosition(transform.position + Movement);
+    }
+}
